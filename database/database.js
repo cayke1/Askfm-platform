08fa6de5b2bd4ed5b9a4728e48c7e490fd556201
cayke1 <68928122+cayke1@users.noreply.github.com>
2021-02-23 01:35:47 -0300

    Update database.js

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const connection = new Sequelize('guiaperguntas','root','@@@g230GG', {
+const connection = new Sequelize('guiaperguntas','root','password', {
     host: 'localhost',
     dialect: 'mysql'
 });
